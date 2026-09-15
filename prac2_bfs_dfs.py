c1b89a35dd03a5a6c67393abba69ff042fdf27c3
Aditi prajapati <aditiprajapati2827@gmail.com>
2026-09-15 20:46:34 +0530

    Add files via upload

---
+++ prac2_bfs_dfs.py
@@ -0,0 +1,63 @@
+from collections import deque
+
+graph = {
+    'A': ['B'],
+    'B': ['A', 'C', 'H'],
+    'C': ['B', 'D'],
+    'D': ['C', 'E', 'G'],
+    'E': ['D', 'F'],
+    'F': ['E'],
+    'G': ['D'],
+    'H': ['B', 'I', 'J', 'M'],
+    'I': ['H'],
+    'J': ['H', 'K'],
+    'K': ['J', 'L'],
+    'L': ['K'],
+    'M': ['H']
+}
+
+# BFS (Breadth First Search)
+# 1. Start from the given node and mark it visited
+# 2. Add it to the queue
+# 3. While queue is not empty, take the front node
+# 4. Visit all unvisited neighbours of that node
+def bfs(graph, start):
+    visited = set()          # keeps track of visited nodes
+    queue = deque()          # queue for BFS
+    visited.add(start)       # mark start as visited
+    queue.append(start)      # push start into queue
+    result = []              # stores traversal order
+
+    while queue:
+        node = queue.popleft()      # remove front node
+        result.append(node)         # add it to result
+        for neighbour in graph[node]:       # check all neighbours
+            if neighbour not in visited:    # if not visited
+                visited.add(neighbour)      # mark visited
+                queue.append(neighbour)     # add to queue
+    return result
+print("BFS Traversal:", bfs(graph, 'A'))
+
+# DFS (Depth First Search)
+# 1. Start from the given node and mark it visited
+# 2. Add it to the stack
+# 3. While stack is not empty, take the top node
+# 4. Visit all unvisited neighbours of that node
+def dfs(graph, start):
+    visited = set()          # keeps track of visited nodes
+    stack = [start]          # stack for DFS
+    visited.add(start)       # mark start as visited
+    result = []              # stores traversal order
+
+    while stack:
+        node = stack.pop()              # remove top node
+        result.append(node)             # add it to result
+        for neighbour in graph[node]:   # check all neighbours
+            if neighbour not in visited:    # if not visited
+                visited.add(neighbour)      # mark visited
+                stack.append(neighbour)     # add to stack
+    return result
+
+print("DFS Traversal:", dfs(graph, 'A'))
+
+
